30546c8ed609e925dd95902c50a4d5fa7517312a
wong nok yin <wongnokyin@wongs-mbp.lan>
2019-08-17 13:31:13 +0800

    make it better

--- index.js
+++ index.js
@@ -10,4 +10,31 @@ fs.readdir('./events/', (err, files) => {
   })
 })
 
+client.on('message', msg => {
+  if (msg.content === 'pum') {
+    msg.reply('Ping!')
+  }
+})
+
+client.on('message', message => {
+  if (message.content.startsWith('!kick')) {
+    const member = message.mentions.members.first()
+
+    if (!member) {
+      return message.reply(
+        `Who are you trying to kick? You must mention a user.`
+      )
+    }
+
+    if (!member.kickable) {
+      return message.reply(`I can't kick this user. Sorry!`)
+    }
+
+    return member
+      .kick()
+      .then(() => message.reply(`${member.user.tag} was kicked.`))
+      .catch(error => message.reply(`Sorry, an error occured.`))
+  }
+})
+
 client.login(process.env.BOT_TOKEN)
